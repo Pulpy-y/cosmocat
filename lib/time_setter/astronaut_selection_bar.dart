954c0616e002e85fbe8e4381587271b0e9ea7cfa
Chu Jia-Cheng <dorazhu@outlook.com>
2021-05-27 22:59:17 +0800

    add image and button to change astronaut

--- lib/time_setter/astronaut_selection_bar.dart
+++ lib/time_setter/astronaut_selection_bar.dart
@@ -1,3 +1,4 @@
+import 'package:cosmocat/size_config.dart';
 import 'package:flutter/material.dart';
 
 class AstronautSelectionBar extends StatefulWidget {
@@ -8,8 +9,43 @@ class AstronautSelectionBar extends StatefulWidget {
 class _AstronautSelectionBarState extends State<AstronautSelectionBar> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
-    );
+    double defaultSize = SizeConfig.defaultSize!;
+    String selectedAnimal = "assets/image/animal_profile/pig_profile.png";
+
+    return Padding(
+        padding: EdgeInsets.all(defaultSize * 1.6),
+        child: Column(children: <Widget>[
+          Container(
+              child: Text(
+                "Who's your astronaut today?",
+                style: TextStyle(
+                  fontSize: defaultSize * 2.2, // 22
+                ),
+                textAlign: TextAlign.left,
+              ),
+              margin: EdgeInsets.all(defaultSize * 1.8) // 18
+              ),
+          Row(
+            children: [
+              Container(
+                margin: EdgeInsets.only(bottom: defaultSize), //10
+                height: defaultSize * 14, //140
+                width: defaultSize * 14,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: Colors.white,
+                    width: defaultSize * 0.8, //8
+                  ),
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage(selectedAnimal),
+                  ),
+                ),
+              ),
+              TextButton(onPressed: null, child: Text("change"))
+            ],
+          )
+        ]));
   }
 }
