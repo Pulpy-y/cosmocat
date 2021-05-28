378d8626764d27837e76bef181353f6377fcc7da
Yao Yi <yaoyi2000326@gmail.com>
2021-05-28 22:29:32 +0800

    deleted useless code

--- lib/log_in.dart
+++ lib/log_in.dart
@@ -2,29 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'home/home_page.dart';
 
-/*
-class Login extends StatefulWidget {
-  @override
-  _LoginDemoState createState() => _LoginDemoState();
-}
-
-class _LoginDemoState extends State<Login> {
-  @override
-  Widget build(BuildContext context) {
-    return TextButton(
-        style: TextButton.styleFrom(
-          padding: const EdgeInsets.all(16.0),
-          primary: Colors.blue[100],
-          textStyle: const TextStyle(fontSize: 20),
-        ),
-        onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (_) => HomePage()));
-        },
-        child: Text("Login"));
-  }
-}*/
-
 class LoginScreen extends StatefulWidget {
   @override
   _LoginScreenState createState() => _LoginScreenState();
