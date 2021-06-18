fbcb3a7e9f8582b57df1992b86714c6e7e23c246
Chu Jia-Cheng <dorazhu@outlook.com>
2021-06-18 15:41:22 +0800

    create animal list

---
+++ lib/animals/animal.dart
@@ -0,0 +1,13 @@
+class Animal {
+  String id;
+  String name;
+
+  Animal(this.id, this.name);
+}
+
+List<Animal> animalList = [
+  Animal("0", "piggy"),
+  Animal("1", "1"),
+  Animal("2", "2"),
+  Animal("3", "3"),
+];
