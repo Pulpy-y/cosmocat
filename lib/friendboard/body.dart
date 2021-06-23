26e230a363b03eaef4fa29e5895a1953f0e8d067
Chu Jia-Cheng <dorazhu@outlook.com>
2021-06-23 22:30:06 +0800

    rough placement

--- lib/friendboard/body.dart
+++ lib/friendboard/body.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
-
+import '../size_config.dart';
 import '../backgroud.dart';
 
 class Body extends StatelessWidget {
+  Set<String> friendList = {"id1", "id2"};
+  double screenHeight = SizeConfig.screenHeight!;
   @override
   Widget build(BuildContext context) {
     return Background(
-        child: Container(
-      child: null,
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Container(
+          height: screenHeight * 2 / 11,
+        ),
+        Container(
+          height: screenHeight * 7 / 11,
+          child: ListView.builder(
+              itemCount: friendList.length,
+              itemBuilder: (context, index) {
+                return Container();
+              }),
+          decoration: BoxDecoration(color: Colors.black),
+        ),
+        Container(
+          height: screenHeight * 2 / 11,
+          decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.5),
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.only(
+                  topLeft: const Radius.circular(40.0),
+                  topRight: const Radius.circular(40.0))),
+        )
+      ],
     ));
   }
 }
--- lib/friendboard/friendboard_page.dart
+++ lib/friendboard/friendboard_page.dart
@@ -6,14 +6,31 @@ class Friendboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
-    return Scaffold(
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 2,
+      child: Scaffold(
         extendBodyBehindAppBar: true,
         appBar: AppBar(
-          title: Text("Friendboard"),
+          title: const Text('Friendboard'),
           centerTitle: true,
           backgroundColor: Colors.transparent,
           elevation: 0,
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(
+                text: "Day",
+              ),
+              Tab(
+                text: "Week",
+              ),
+            ],
+          ),
         ),
-        body: Body());
+        body: TabBarView(
+          children: <Widget>[Body(), Body()], //day and week, need to pass vars
+        ),
+      ),
+    );
   }
 }
