03bd1d77c32b0b0266b3e3b152d45e3f4b93d15e
Chu Jia-Cheng <dorazhu@outlook.com>
2021-07-14 17:15:53 +0800

    add todo model

---
+++ lib/models/todo_model.dart
@@ -0,0 +1,10 @@
+class ToDoModel {
+  String startDatetime; //this will be an unique id of every todoTask
+  String category;
+  String durationHour;
+  String durationMinute;
+  String austronautId;
+
+  ToDoModel(this.startDatetime, this.category, this.durationHour,
+      this.durationMinute, this.austronautId);
+}
