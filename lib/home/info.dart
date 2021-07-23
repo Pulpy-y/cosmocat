bb204fb95afbb68c994ea521b70560f082387947
Chu Jia-Cheng <dorazhu@outlook.com>
2021-07-23 23:27:49 +0800

    able to change user profile

--- lib/home/info.dart
+++ lib/home/info.dart
@@ -1,3 +1,5 @@
+import 'package:cosmocat/Login/log_in.dart';
+import 'package:cosmocat/database.dart';
 import 'package:flutter/material.dart';
 import '../size_config.dart';
 
@@ -14,16 +16,22 @@ class Info extends StatefulWidget {
 }
 
 class _InfoState extends State<Info> {
+  double defaultSize = SizeConfig.defaultSize!;
+  double defualtHeight = SizeConfig.screenHeight! / 10;
   String name;
   String userAnimal;
+  List<String> astronautIdList = ["0"];
 
   _InfoState(this.name, this.userAnimal);
 
   @override
-  Widget build(BuildContext context) {
-    double defaultSize = SizeConfig.defaultSize!;
-    double defualtHeight = SizeConfig.screenHeight! / 10;
+  void initState() {
+    super.initState();
+    getAnimalList();
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return SizedBox(
         height: defualtHeight * 3, // 240
         child: Stack(children: <Widget>[
@@ -32,6 +40,9 @@ class _InfoState extends State<Info> {
               mainAxisAlignment: MainAxisAlignment.end,
               children: <Widget>[
                 Container(
+                  child: InkWell(
+                    onTap: () => _selectAnimalDialog(),
+                  ),
                   margin: EdgeInsets.only(bottom: defaultSize), //10
                   height: defaultSize * 14, //140
                   width: defaultSize * 14,
@@ -61,5 +72,42 @@ class _InfoState extends State<Info> {
             ),
           )
         ]));
+  }
+
+  Future<void> getAnimalList() async {
+    astronautIdList = await DatabaseService().getAnimalList(user!.uid);
+  }
+
+  Future<void> _selectAnimalDialog() async {
+    List<Widget> tiles = astronautIdList.map((e) => _buildGridTile(e)).toList();
+
+    return showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (_) => AlertDialog(
+            title: const Text("Choose your profile animal ฅ•ω•ฅ"),
+            insetPadding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 100),
+            contentPadding: EdgeInsets.zero,
+            content: Container(
+                height: defualtHeight * 5,
+                width: defaultSize,
+                child: GridView.count(
+                  crossAxisCount: 3,
+                  children: tiles,
+                ))));
+  }
+
+  Widget _buildGridTile(String id) {
+    String path = "assets/image/animal_floating/$id.png";
+    return SimpleDialogOption(
+      child: Container(padding: EdgeInsets.zero, child: Image.asset(path)),
+      onPressed: () {
+        setState(() {
+          userAnimal = id;
+          DatabaseService().updateProfileAnimal(id);
+        });
+        Navigator.pop(context);
+      },
+    );
   }
 }
