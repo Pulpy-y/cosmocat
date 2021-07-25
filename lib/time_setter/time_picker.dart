5e63d51e660898336dfb2a8e1f937d5cb016ee83
Yao Yi <yaoyi2000326@gmail.com>
2021-07-26 00:44:57 +0800

    fixed tag duration hidden by keyboard

--- lib/time_setter/time_picker.dart
+++ lib/time_setter/time_picker.dart
@@ -24,78 +24,80 @@ class _TimePickerState extends State<TimePicker> {
     return Padding(
       padding: EdgeInsets.symmetric(
           vertical: defaultSize, horizontal: defaultSize * 1.6),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                    child: Text(
-                      "Focus Duration",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: defaultSize * 2.2, //22
-                          color: Colors.white),
-                      textAlign: TextAlign.left,
-                    ),
-                    margin: EdgeInsets.all(defaultSize * 1.8) // 18
-                    ),
-                Card(
-                    // input time
-                    color: themeSecondaryColor,
-                    child: Padding(
-                        padding: EdgeInsets.all(defaultSize * 1.6), //16
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Container(
-                              width: defaultSize * 15,
-                              child: TextField(
-                                keyboardType: TextInputType.number,
-                                inputFormatters: <TextInputFormatter>[
-                                  FilteringTextInputFormatter.digitsOnly
-                                ], //only numbers can be entered
-                                decoration: InputDecoration(
-                                    border: OutlineInputBorder(),
-                                    labelText: 'Hours',
-                                    fillColor: Colors.white),
-                                onChanged: (value) {
-                                  if (value == "") hour = 0;
-                                  hour = int.parse(value.trim());
-                                },
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                      child: Text(
+                        "Focus Duration",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: defaultSize * 2.2, //22
+                            color: Colors.white),
+                        textAlign: TextAlign.left,
+                      ),
+                      margin: EdgeInsets.all(defaultSize * 1.8) // 18
+                      ),
+                  Card(
+                      // input time
+                      color: themeSecondaryColor,
+                      child: Padding(
+                          padding: EdgeInsets.all(defaultSize * 1.6), //16
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Container(
+                                width: defaultSize * 15,
+                                child: TextField(
+                                  keyboardType: TextInputType.number,
+                                  inputFormatters: <TextInputFormatter>[
+                                    FilteringTextInputFormatter.digitsOnly
+                                  ], //only numbers can be entered
+                                  decoration: InputDecoration(
+                                      border: OutlineInputBorder(),
+                                      labelText: 'Hours',
+                                      fillColor: Colors.white),
+                                  onChanged: (value) {
+                                    if (value == "") hour = 0;
+                                    hour = int.parse(value.trim());
+                                  },
+                                ),
                               ),
-                            ),
-                            Container(
-                                margin: EdgeInsets.all(defaultSize * 1.8),
-                                child: Text(
-                                  ":",
-                                  style: TextStyle(fontSize: defaultSize * 2),
-                                )),
-                            Container(
-                              width: defaultSize * 15,
-                              child: TextField(
-                                keyboardType: TextInputType.number,
-                                inputFormatters: <TextInputFormatter>[
-                                  FilteringTextInputFormatter.digitsOnly
-                                ], //only numbers can be entered
-                                decoration: InputDecoration(
-                                    border: OutlineInputBorder(),
-                                    labelText: 'Minutes',
-                                    fillColor: Colors.white),
-                                onChanged: (value) {
-                                  if (value == "") minute = 0;
-                                  minute = int.parse(value.trim());
-                                },
+                              Container(
+                                  margin: EdgeInsets.all(defaultSize * 1.8),
+                                  child: Text(
+                                    ":",
+                                    style: TextStyle(fontSize: defaultSize * 2),
+                                  )),
+                              Container(
+                                width: defaultSize * 15,
+                                child: TextField(
+                                  keyboardType: TextInputType.number,
+                                  inputFormatters: <TextInputFormatter>[
+                                    FilteringTextInputFormatter.digitsOnly
+                                  ], //only numbers can be entered
+                                  decoration: InputDecoration(
+                                      border: OutlineInputBorder(),
+                                      labelText: 'Minutes',
+                                      fillColor: Colors.white),
+                                  onChanged: (value) {
+                                    if (value == "") minute = 0;
+                                    minute = int.parse(value.trim());
+                                  },
+                                ),
                               ),
-                            ),
-                          ],
-                        ))),
-              ]),
-          SizedBox(
-            height: 10,
-          ),
-        ],
+                            ],
+                          ))),
+                ]),
+            SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
       ),
     );
   }
